a449b54b9352c1fe2cd008c985048d00065de765
Joe Chau <83691049+joechau09021@users.noreply.github.com>
2022-12-21 18:18:08 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/C6BmKX7z9hnbfX-n/scene.splinecode');
